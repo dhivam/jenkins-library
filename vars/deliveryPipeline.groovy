939a34ec4ba7af5b89ae9ed7577baacfd4888be3
dhivam <74132851+dhivam@users.noreply.github.com>
2021-09-17 16:04:21 +0700

    Update deliveryPipeline.groovy

--- vars/deliveryPipeline.groovy
+++ vars/deliveryPipeline.groovy
@@ -1,77 +1,27 @@
 #!/usr/bin/env groovy
 
-
-def call(Map param){
-	pipeline {
-		agent any
-		stages {
-			stage ("telegram notif"){
-                agent {
-					label "dockerworker"
-				}
-				steps{
-					echo "${getMessage()} ${param.text}"
-				}
-			}
-			stage('Build') {
-                agent {
-					label "dockerworker"
-				}
-				steps {
-					sh 'mvn -B -DskipTests clean package'
-				}
-			}
-			stage('Test') {
-                agent {
-					label "dockerworker"
-				}
-				steps {
-					sh 'mvn test'
-				}
-				post {
-					always {
-						junit 'target/surefire-reports/*.xml'
-					}
-				}
-			}
-		}
-
-		stages {
-			stage ("telegram notif"){
-                agent {
-					label "worker"
-				}
-				steps{
-					echo "${getMessage()} ${param.text}"
-				}
-			}
-			stage('Build') {
-                agent {
-					label "worker"
-				}
-				steps {
-					sh 'mvn -B -DskipTests clean package'
-				}
-			}
-			stage('Test') {
-                agent {
-					label "worker"
-				}
-				steps {
-					sh 'mvn test'
-				}
-				post {
-					always {
-						junit 'target/surefire-reports/*.xml'
-					}
-				}
-			}
-		}
+def call(String agent) {
+    pipeline {
+    agent none
+    stages {
+        stage('Even Stage') {
+            when {
+                expression { ${params.agent} == 'any' }
+            }
+            agent {label 'dockerworker'} 
+            steps {
+                echo "The build number is even"
+            }
+        }
+        stage('Odd Stage') {
+            when {
+                expression { !${params.agent} == 'any' }
+            }
+            agent {label 'worker2'} 
+            steps {
+                echo "The build number is odd"
+            }
+        }
     }
 }
-
-def getMessage (){
-	def commiter = sh(script: "git show -s --pretty=%cn",returnStdout: true).trim()
-	def message = "$commiter deploying app"
-	return message
 }
